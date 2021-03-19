7668d6c8cf44d67b4331e86d5e395ce0ef7ee152
SirdsH <svatopluk.houserek@educanet.cz>
2021-03-19 11:15:12 +0100

    commit

---
+++ js.js
@@ -0,0 +1,23 @@
+const board = document.querySelector(".board");
+let index = 0;
+let black = false;
+
+for (let i = 1; i <= 64; i++) {
+    const square = document.createElement("div");
+    if (black) {
+        square.classList.add("square");
+        square.classList.add("black");
+        index++;
+        black = !black;
+    } else {
+        square.classList.add("square");
+        square.classList.add("white");
+        index++;
+        black = !black;
+    }
+    board.appendChild(square);
+    if (index === 8) {
+        black = !black;
+        index = 0;
+    }
+}
